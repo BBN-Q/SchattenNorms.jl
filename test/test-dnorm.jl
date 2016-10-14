994f88495b2287078bb7abab0a2cbbb6311a1733
Marcus P S <marcusps@users.noreply.github.com>
2016-10-13 21:47:48 -0400

    Added tests for dnorm of difference of unitaries

--- test/test-dnorm.jl
+++ test/test-dnorm.jl
@@ -32,19 +32,43 @@ v = liou(rand_unitary(2))
 #ff = rand_cp_map(2)
 
 @test_approx_eq_eps 2.0 dnorm(cnotL-eye(16)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(cnotL,eye(16)) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(eye(4)-liou(x,x)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(eye(2),x) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(eye(4)-liou(z,z)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(eye(4),z) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(eye(4)-liou(y,y)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(eye(2),y) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(liou(x,x)-liou(y,y)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(x,y) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(liou(x,x)-liou(z,z)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(x,z) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(liou(z,z)-liou(y,y)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(z,y) 1e-5
 
 @test_approx_eq_eps 2.0 dnorm(u*eye(4)-u*liou(x,x)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(u*eye(4),u*liou(x,x)) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(u*eye(4)-u*liou(z,z)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(u*eye(4),u*liou(z,z)) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(u*eye(4)-u*liou(y,y)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(u*eye(4),u*liou(y,y)) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(u*liou(x,x)-u*liou(y,y)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(u*liou(x,x),u*liou(y,y)) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(u*liou(x,x)-u*liou(z,z)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(u*liou(x,x),u*liou(z,z)) 1e-5
+
 @test_approx_eq_eps 2.0 dnorm(u*liou(z,z)-u*liou(y,y)) 1e-5
+@test_approx_eq_eps 2.0 dnorm(u*liou(z,z),u*liou(y,y)) 1e-5
 
 pc1 = sum([ p1[ii+1]*liou(pauli[ii+1]) for ii in 0:3])
 pc2 = sum([ p2[ii+1]*liou(pauli[ii+1]) for ii in 0:3])
@@ -58,6 +82,19 @@ duv  = dnorm(u-v)
     duv3 = dnorm(v'u-eye(4))
     duv4 = dnorm(eye(4)-u'*v)
     duv5 = dnorm(eye(4)-v*u')    
+end
+
+@test_approx_eq_eps duv duv2 1e-5
+@test_approx_eq_eps duv duv3 1e-5
+@test_approx_eq_eps duv duv4 1e-5
+@test_approx_eq_eps duv duv5 1e-5
+
+duv  = dnorm(u,v)
+@time begin 
+    duv2 = dnorm(u*v',eye(4))
+    duv3 = dnorm(v'u,eye(4))
+    duv4 = dnorm(eye(4),u'*v)
+    duv5 = dnorm(eye(4),v*u')    
 end
 
 @test_approx_eq_eps duv duv2 1e-5
