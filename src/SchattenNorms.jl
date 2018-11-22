c5384286ba1e5fae3ce7560b6ed19c1bb4049217
John Lapeyre <jlapeyre@users.noreply.github.com>
2018-11-22 16:12:45 +0100

    Remove commented out precompilation line
    Precompilation is the default now. We may need to specify that
    SchattenNorms.jl *not* be compiled.
    
    	modified:   src/SchattenNorms.jl

--- src/SchattenNorms.jl
+++ src/SchattenNorms.jl
@@ -12,8 +12,6 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
-# precompilation disabled because of the Convex.jl dependency
-# VERSION >= v"0.4.0" && __precompile__()
 module SchattenNorms
 import LinearAlgebra
 
